cfacaae69bd0e9ea4584eb4cb9605b0e4a16f65e
nat <nathalia.n.reis@aluno.senai.br>
2025-09-13 17:52:21 -0300

    feat: deletar Barbie

--- src/controllers/barbieController.js
+++ src/controllers/barbieController.js
@@ -10,41 +10,64 @@ const getAll = (req, res) => {
 
 let getById = (req, res) => {
   const id = parseInt(req.params.id);
-  const barbie = barbies.find(b => b.id === id)
+  const barbie = barbies.find((b) => b.id === id);
 
-if (barbie) {
-  res.status(200).json(barbie)
-} else {
-  res.status(404).json({
-    mensagem: "Essa profissão da Barbie não existe"
-  })
+  if (barbie) {
+    res.status(200).json(barbie);
+  } else {
+    res.status(404).json({
+      mensagem: "Essa profissão da Barbie não existe",
+    });
   }
-}
+};
 
 let createBarbie = (req, res) => {
-  const {nome, profissao, anoLancamento} = req.body;
+  const { nome, profissao, anoLancamento } = req.body;
 
   if (!nome || !profissao) {
     return res.status(400).json({
       sucess: false,
-      message: "Nome e profissão são campos obrigatórios!"
-    })
+      message: "Nome e profissão são campos obrigatórios!",
+    });
   }
 
   const newBarbie = {
     id: barbies.length++,
     nome: nome,
     profissao: profissao,
-    anoLancamento: anoLancamento
-  }
-  
-  barbies.push(newBarbie)
+    anoLancamento: anoLancamento,
+  };
+
+  barbies.push(newBarbie);
 
   res.status(200).json({
     sucess: true,
     message: "Barbie criada com sucesso!",
-    data: newBarbie
-  })
-}
+    data: newBarbie,
+  });
+};
 
-export { getAll, getById, createBarbie };
+let deletarBarbie = (req, res) => {
+  let id = parseInt(req.params.id);
+
+  const barbieParaDeletar = barbies.find((b) => b.id === id);
+
+  if (!barbieParaDeletar) {
+    return res.status(404).json({
+      sucess: false,
+      message: `Esse ID da barbie não existe: ${id}`,
+    });
+  }
+
+  const barbiesFiltradas = barbies.filter((barbie) => barbie.id !== id);
+
+  barbies.splice(0, barbies.length, ...barbiesFiltradas);
+
+  res.status(200).json({
+    sucess: true,
+    message: "Barbie deletada com sucesso!",
+    barbieDeletada: barbieParaDeletar,
+  });
+};
+
+export { getAll, getById, createBarbie, deletarBarbie };
--- src/routes/barbieRoutes.js
+++ src/routes/barbieRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getAll, getById, createBarbie} from "../controllers/barbieController.js";
+import {
+  getAll,
+  getById,
+  createBarbie,
+  deletarBarbie,
+} from "../controllers/barbieController.js";
 
 const router = express.Router();
 
 router.get("/", getAll);
 router.get("/:id", getById);
 router.post("/", createBarbie);
+router.delete("/:id", deletarBarbie);
 
 export default router;
